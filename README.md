a55f9de6423b2e342c443f7a6798494597b52a1a
Carmen To <carmen_to@ymail.com>
2017-09-16 14:47:13 -0700

    finished setup for js development

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quokka
+
+## `npm` dependencies
+`babel-plugin-transform-class-properties` : no more binding for handlers
+`axios` - no more APIUtil
+`material-ui` - styling library
+`prop-types` - linter to check for required props
+
+## files
+`.eslint.json` - custom linter rules
+`babelrc` - setup presets translation
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '/frontend/Application.jsx'),
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: { loader: 'babel-loader' }
+      },
+    ]
+  },
+  devtool: 'source-maps'
+};
