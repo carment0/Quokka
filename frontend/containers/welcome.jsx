71858591c9b59b144b960d0f76ad94c85c428fcd
Carmen To <carmen_to@ymail.com>
2017-09-20 21:26:33 -0700

    refactor welcome

--- frontend/containers/welcome.jsx
+++ frontend/containers/welcome.jsx
@@ -1,108 +1,17 @@
-import Modal from 'react-modal';
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { login, signup, logout } from '../actions/session_actions';
+import Login from '../components/login';
+import Signup from '../components/signup';
 
 class Welcome extends React.Component {
-  state = {
-    username: '',
-    password: '',
-    modalIsOpen: false,
-    formType: ''
-  };
-
-  static propTypes = {
-    dispatchSignup: PropTypes.func.isRequired,
-    dispatchLogin: PropTypes.func.isRequired
-  }
-
-  update(field) {
-    return (e) => this.setState({
-      [field]: e.currentTarget.value
-    });
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    if (this.state.formType === 'Sign Up') {
-      this.props.dispatchSignup(this.state);
-    } else {
-      this.props.dispatchLogin(this.state);
-    }
-  }
-
-  openForm = (formType) => {
-    this.setState({
-      modalIsOpen: true,
-      formType
-    });
-  }
-
-  closeForm = () => {
-    this.setState({
-      modalIsOpen: false,
-      formType: '',
-      username: '',
-      password: ''
-    });
-  }
-
-  toggleFormLink = () => {
-    if (this.state.formType === 'Sign Up') {
-      return (
-        <button onClick={this.toggleForm}>
-          Already have an account? Log in
-        </button>
-      );
-    }
-
-    return (
-      <button onClick={this.toggleForm}>
-        {"Don't have an account? Sign up"}
-      </button>
-    );
-  }
-
-  toggleForm = () => {
-    if (this.state.formType === 'Sign Up') {
-      this.setState({ formType: 'Log In' });
-    } else {
-      this.setState({ formType: 'Sign Up' });
-    }
-  }
-
   render() {
     return (
-      <div className="session-form-container">
-        <nav className="signup-login">
-          <button onClick={() => this.openForm('Log In')}>Log In</button>
-          <button onClick={() => this.openForm('Sign Up')}>Sign Up</button>
+      <div className="welcome-container">
+        <nav className="signup-login-components">
+          <button onClick={<Login/>}>Log In</button>
+          <button onClick={<Signup/>}>Sign Up</button>
         </nav>
-
-        <Modal isOpen={this.state.modalIsOpen} contentLabel="Modal">
-          <button onClick={() => this.closeForm()}>
-            <i className="close_modal" aria-hidden="true" />
-          </button>
-
-          <div className="session-form-box">
-            <h2>{this.state.formType}</h2>
-            <form className="session-form" onSubmit={this.handleSubmit}>
-              <input type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
-                placeholder="Username" />
-              <br />
-              <input type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                placeholder="Password" />
-              <br />
-              <input type="submit" value={this.state.formType} />
-            </form>
-            {this.toggleFormLink()}
-          </div>
-        </Modal>
       </div>
     );
   }
@@ -110,7 +19,7 @@ class Welcome extends React.Component {
 
 const mapStateToProps = (state) => ({
   loggedIn: Boolean(state.session.currentUser),
-  errors: state.errors.sessionErrors
+  errors: state.errors.session
 });
 
 const mapDispatchToProps = (dispatch) => ({
