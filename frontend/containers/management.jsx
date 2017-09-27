f0284400a7fcac7c134a64f33eab90a9b1ec4e8c
Carmen To <carmen.j.to@gmail.com>
2017-09-27 00:36:46 -0700

    added a side bar that is dynamically sliding left and right

--- frontend/containers/management.jsx
+++ frontend/containers/management.jsx
@@ -1,43 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { connect } from 'react-redux';
+
+import Drawer from 'material-ui/Drawer';
+import RaisedButton from 'material-ui/RaisedButton';
+import MenuItem from 'material-ui/MenuItem';
+
+// Actions
 import { logout } from '../actions/session_actions';
 
 // Quokka imports
 import AdministratedProjectIndex from './administrated_project_index';
 import AssignedProjectIndex from './assigned_project_index';
 
-// Routes
 
 class Management extends React.Component {
+  state = {
+    sidebarOpen: false
+  };
+
   static propTypes = {
     dispatchLogout: PropTypes.func.isRequired,
     currentUser: PropTypes.object.isRequired
   };
 
-  componentWillMount() {
-    // Redirect user if no one is currently signed in
+  handleSidebarOpen = () => {
+    this.setState({
+      sidebarOpen: !this.state.sidebarOpen
+    });
+  }
+
+  handleLogout = () => {
+    this.props.dispatchLogout();
+  }
+
+  get managementMainContentClassName() {
+    if (this.state.sidebarOpen) {
+      return 'main-content content-with-menu-open';
+    }
+
+    return 'main-content';
+  }
+
+  get sidebarClassName() {
+    if (this.state.sidebarOpen) {
+      return 'left-side-bar left-side-bar-with-menu-open';
+    }
+
+    return 'left-side-bar';
   }
 
   render() {
     return (
       <div className="management">
-        <div className="management-main">
-          <div className="left-sidebar">
-            <div className="user-project-sidebar">Project Sidebar</div>
-            <div className="user-notification-sidebar">My Notifications</div>
-          </div>
-          <div className="right-container">
-            <h2 className="header-name">Hi, {this.props.currentUser.username}!</h2>
-            <button className="header-button" onClick={() => this.props.dispatchLogout()}>Log out</button>
-            <AdministratedProjectIndex />
-            <AssignedProjectIndex />
-          </div>
+        <div className={this.sidebarClassName}>
+          <Drawer open={this.state.sidebarOpen} swipeAreaWidth={50}>
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
         </div>
-        <div className="management-footer">
-          <div className="icons" id="1">github icon</div>
-          <div className="icons" id="2">linkedin icon</div>
+        <div className={this.managementMainContentClassName}>
+          <h2 className="header-name">Hi, {this.props.currentUser.username}!</h2>
+          <RaisedButton label="Log out" onClick={this.handleLogout} />
+          <RaisedButton label="Menu" onClick={this.handleSidebarOpen} />
+          <AdministratedProjectIndex />
+          <AssignedProjectIndex />
         </div>
       </div>
     );
