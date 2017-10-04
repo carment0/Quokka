f7fd30439f4984496234910a11c2188daee46c40
Carmen To <carmen.j.to@gmail.com>
2017-10-03 23:42:55 -0700

    now tasks actually save and update when users click

--- frontend/actions/task_actions.js
+++ frontend/actions/task_actions.js
@@ -1,54 +1,57 @@
-export const RECEIVE_TASKS = 'RECEIVE_TASKS';
 export const RECEIVE_TASK = 'RECEIVE_TASK';
-export const REMOVE_TASK = 'REMOVE_TASK';
-export const RECEIVE_TASK_ERRORS = 'RECEIVE_TASK_ERRORS';
-export const RECEIVE_ASSIGNED_TASKS = 'RECEIVE_ASSIGNED_TASKS';
-
-
-export const receiveTasks = (tasks) => ({
-  type: RECEIVE_TASKS,
-  tasks
-});
-
-export const receiveAssignedTasks = (tasks) => ({
-  type: RECEIVE_ASSIGNED_TASKS,
-  tasks
-});
-
-
 export const receiveTask = (task) => ({
   type: RECEIVE_TASK,
   task
 });
 
+export const RECEIVE_TASK_LIST = 'RECEIVE_TASK_LIST';
+export const receiveTaskList = (taskList) => ({
+  type: RECEIVE_TASK_LIST,
+  taskList
+});
+
+export const RECEIVE_ASSIGNED_TASK = 'RECEIVE_ASSIGNED_TASK';
+export const receiveAssignedTask = (task) => ({
+  type: RECEIVE_ASSIGNED_TASK,
+  task
+});
+
+export const RECEIVE_ASSIGNED_TASK_LIST = 'RECEIVE_ASSIGNED_TASK_LIST';
+export const receiveAssignedTaskList = (taskList) => ({
+  type: RECEIVE_ASSIGNED_TASK_LIST,
+  taskList
+});
+
+export const REMOVE_TASK = 'REMOVE_TASK';
 export const removeTask = (task) => ({
   type: REMOVE_TASK,
   task
 });
 
-export const receiveErrors = (errors) => ({
+export const RECEIVE_TASK_ERRORS = 'RECEIVE_TASK_ERRORS';
+export const receiveTaskErrors = (errors) => ({
   type: RECEIVE_TASK_ERRORS,
   errors
 });
-
-export const fetchTasks = (project_id) => (dispatch) => (
-  $.ajax({ method: 'GET', url: `/api/projects/${project_id}/tasks` })
-    .then((tasks) => (
-      dispatch(receiveTasks(tasks))
-    ))
-);
-
-export const fetchAssignedTasks = (userId) => (dispatch) => (
-  $.ajax({ method: 'GET', url: `/api/users/${userId}/tasks/assigned` })
-    .then((taskList) => (
-      dispatch(receiveAssignedTasks(taskList))
-    ))
-);
 
 export const fetchTask = (project_id, task_id) => (dispatch) => (
   $.ajax({ method: 'GET', url: `/api/projects/${project_id}/tasks/${task_id}` })
     .then((task) => (
       dispatch(receiveTask(task))
+    ))
+);
+
+export const fetchTaskList = (project_id) => (dispatch) => (
+  $.ajax({ method: 'GET', url: `/api/projects/${project_id}/tasks` })
+    .then((tasks) => (
+      dispatch(receiveTaskList(tasks))
+    ))
+);
+
+export const fetchAssignedTaskList = (userId) => (dispatch) => (
+  $.ajax({ method: 'GET', url: `/api/users/${userId}/tasks/assigned` })
+    .then((taskList) => (
+      dispatch(receiveAssignedTaskList(taskList))
     ))
 );
 
@@ -58,7 +61,7 @@ export const createTask = (project_id, new_task) => (dispatch) => (
       dispatch(receiveTask(task))
     ))
     .fail((err) => (
-      dispatch(receiveErrors(err.responseJSON))
+      dispatch(receiveTaskErrors(err.responseJSON))
     ))
 );
 
@@ -68,7 +71,17 @@ export const updateTask = (task) => (dispatch) => (
       dispatch(receiveTask(updatedTask))
     ))
     .fail((err) => (
-      dispatch(receiveErrors(err.responseJSON))
+      dispatch(receiveTaskErrors(err.responseJSON))
+    ))
+);
+
+export const updateAssignedTask = (task) => (dispatch) => (
+  $.ajax({ method: 'PATCH', url: `api/projects/${task.project_id}/tasks/${task.id}`, data: { task } })
+    .then((updatedTask) => (
+      dispatch(receiveAssignedTask(updatedTask))
+    ))
+    .fail((err) => (
+      dispatch(receiveTaskErrors(err.responseJSON))
     ))
 );
 
--- frontend/reducers/tasks/assigned.js
+++ frontend/reducers/tasks/assigned.js
@@ -1,11 +1,21 @@
-import { RECEIVE_ASSIGNED_TASKS } from '../../actions/task_actions';
+import { merge } from 'lodash';
+import { RECEIVE_ASSIGNED_TASK_LIST, RECEIVE_ASSIGNED_TASK } from '../../actions/task_actions';
 
-export default (oldState = [], action) => {
+export default (oldState = {}, action) => {
   Object.freeze(oldState);
 
   switch (action.type) {
-    case RECEIVE_ASSIGNED_TASKS:
-      return action.tasks;
+    case RECEIVE_ASSIGNED_TASK_LIST:
+      const newState = {};
+
+      action.taskList.forEach((task) => {
+        newState[task.id] = task;
+      });
+
+      return newState;
+
+    case RECEIVE_ASSIGNED_TASK:
+      return merge({}, oldState, { [action.task.id]: action.task });
 
     default:
       return oldState;
